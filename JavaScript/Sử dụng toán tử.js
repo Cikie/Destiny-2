a7bcfeed23e1674706112da1557e354b9d67abae
Cikie <daoanh123123@gmail.com>
2022-08-04 16:47:04 +0700

    Toan tu

---
+++ JavaScript/Sử dụng toán tử.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Chiều cao");
+inputHeight = prompt("Chiều ngang");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is: " + area);
