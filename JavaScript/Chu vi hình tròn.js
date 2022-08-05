43416511255f322933cecb6f879400654fb65d80
Cikie <daoanh123123@gmail.com>
2022-08-05 09:27:57 +0700

    Chu Vi

---
+++ JavaScript/Chu vi hình tròn.js
@@ -0,0 +1,10 @@
+ function TinhChuvi(){
+    let r =document.getElementById('r').value;
+    let chuvi = 2 * r * Math.PI;
+    alert ('Chu Vi Hinh Tron = :' + chuvi);
+ }
+ function TinhDienTich() {
+    let r = document.getElementById('r').value;
+    let dientich = r * r * Math.PI;
+    alert('Dien Tich Hinh Tron =' + dientich);
+ }
